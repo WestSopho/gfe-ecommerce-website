de542da3721e55ed3a30e8e941d45aa1bf44d029
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-11-22 20:21:06 -0500

    style: remove py-2 in CustomLink

--- components/ui/CustomLink/CustomLink.tsx
+++ components/ui/CustomLink/CustomLink.tsx
@@ -11,7 +11,7 @@ export default function CustomLink({ text, href }: CustomLinkProps) {
     return (
         <Link
             href={href}
-            className="desktop:text-base desktop:font-[500] focus:shadow-link py-2 text-sm font-medium text-neutral-600 hover:text-neutral-900 focus:rounded-[4px] disabled:text-neutral-400 lg:py-0"
+            className="desktop:text-base desktop:font-[500] text-sm font-medium text-neutral-600 hover:text-neutral-900 focus:rounded-[4px] focus:shadow-link disabled:text-neutral-400 lg:py-0"
         >
             {text}
         </Link>
