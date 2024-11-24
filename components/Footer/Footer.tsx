2a52f014330b9f5a54fa127f67aed62c5159d8fe
WestSopho <153777405+WestSopho@users.noreply.github.com>
2024-11-24 18:34:19 -0500

    feat: implemented copyright and social icons container

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -3,6 +3,14 @@ import Image from "next/image";
 
 import clsx from "clsx";
 
+import {
+    RiYoutubeLine,
+    RiInstagramLine,
+    RiFacebookBoxLine,
+    RiGithubLine,
+    RiTwitterXLine,
+} from "react-icons/ri";
+
 import NewsletterForm from "./NewsletterForm";
 import CustomLink from "../ui/CustomLink/CustomLink";
 
@@ -77,7 +85,7 @@ export default function Footer() {
             <NewsletterForm />
 
             {/* Links */}
-            <section>
+            <section className="flex flex-col gap-12 md:gap-16">
                 {/* Logo, Supporting and Links Container */}
                 <div className="container">
                     {/* Content */}
@@ -115,6 +123,43 @@ export default function Footer() {
                                 links={shopCollections}
                                 header="Shop Collections"
                             />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Copyright and Social Icons Container */}
+                <div className="container">
+                    {/* Content */}
+                    <div className="col-span-full grid grid-cols-subgrid border-t border-solid border-neutral-200 pt-8">
+                        <div className="col-span-full flex flex-col gap-y-8 md:flex-row md:gap-x-8">
+                            {/* Footer Text */}
+                            <p className="text-neutral-500 lg:grow">
+                                &copy; 2024 StyleNest, Inc. All rights reserved.
+                            </p>
+
+                            {/* Social Icons */}
+                            <div className="flex flex-row gap-x-6">
+                                <RiYoutubeLine
+                                    size={24}
+                                    className="fill-neutral-400"
+                                />
+                                <RiInstagramLine
+                                    size={24}
+                                    className="fill-neutral-400"
+                                />
+                                <RiFacebookBoxLine
+                                    size={24}
+                                    className="fill-neutral-400"
+                                />
+                                <RiGithubLine
+                                    size={24}
+                                    className="fill-neutral-400"
+                                />
+                                <RiTwitterXLine
+                                    size={24}
+                                    className="fill-neutral-400"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
